f7721576fa50b15de9645222d9fa62ff4b3a8aca
Trachuk Serhiy <trachukserhiy@gmail.com>
2020-10-24 22:39:37 +0300

    Add phonesPage reducer.

--- src/reducers/phonesPage.js
+++ src/reducers/phonesPage.js
@@ -1,16 +1,19 @@
 import * as R from 'ramda'
 
 import {
-  FETCH_PHONES_SUCCESS,
+  FETCH_PHONES_SUCCESS
 } from '../actions/actionTypes'
 
-const initialState = {}
+const initialState = {
+  ids: [],
+}
 
 export default (state = initialState, {type, payload}) => {
   switch (type) {
     case FETCH_PHONES_SUCCESS:
-      const newValues = R.indexBy(R.prop('id'), payload)
-      return R.mergeRight(state, newValues)
+      return R.mergeRight(state, {
+        ids: R.pluck('id', payload)
+      })
     default:
       return state
   }
